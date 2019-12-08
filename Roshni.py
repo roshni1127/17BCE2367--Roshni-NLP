e770df50da689c6d1775fc323577e585f86562dd
Roshni <53559702+roshni1127@users.noreply.github.com>
2019-12-08 17:52:26 +0530

    Task 1

---
+++ Roshni.py
@@ -0,0 +1,10 @@
+
+
+r='Shakespeare gave the world best of theatrics.'
+print(r)
+s="Shakespeare didn't gave the world best theatrics."
+print(s)
+z= '''Shakespeare was born and raised in Stratford-upon-Avon, Warwickshire. At the age of 18, he married Anne Hathaway, with whom he had three children: Susanna and twins Hamnet and Judith. Sometime between 1585 and 1592, he began a successful career in London as an actor, writer, and part-owner of a playing company called the Lord Chamberlain's Men, later known as the King's Men.'''
+print(z)
+
+
